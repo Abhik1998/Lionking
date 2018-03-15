d77a0c0ca8a4823c2fa3a43c8ca62366c4704abc
Pranav Yerabati <pyerabat@ucsc.edu>
2018-03-14 19:32:01 -0700

    made out files for grey parrot

---
+++ make.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2016.csv --year=2016 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2016.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2015.csv --year=2015 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2015.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2014.csv --year=2014 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2014.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2013.csv --year=2013 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2013.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2012.csv --year=2012 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2012.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2011.csv --year=2011 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2011.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2010.csv --year=2010 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2010.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2009.csv --year=2009 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2009.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2008.csv --year=2008 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2008.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2007.csv --year=2007 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2007.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2006.csv --year=2006 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2006.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2005.csv --year=2005 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2005.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2004.csv --year=2004 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2004.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2003.csv --year=2003 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2003.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2002.csv --year=2002 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2002.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2001.csv --year=2001 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2001.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/2000.csv --year=2000 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_2000.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/1999.csv --year=1999 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_1999.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/1998.csv --year=1998 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_1998.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/1997.csv --year=1997 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_1997.csv
+
+python parse.py --data=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/1996.csv --year=1996 --terms="live" --out=/home/pranav/school/FinalProject/csvFiles/Animals/Grey_Parrot/out_1996.csv
+
